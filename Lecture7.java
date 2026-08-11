4f14d7e9010ecbbdaff68b10545151c3ac9156fb
Sanjali Kumari <sanjali8911@users.noreply.github.com>
2026-08-11 18:36:38 +0530

    Uploded replica to Github

---
+++ Lecture7.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Lecture7 {
+    public static String popla( String sona) {
+
+return sona;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String sonal = sc.next();
+System.out.println(popla(sonal));
+    }
+}
